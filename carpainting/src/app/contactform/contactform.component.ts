b36c0be1cc39104325721e9fd0871c3d5ce2334d
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2024-06-05 22:05:46 +0200

    Update contactform

---
+++ carpainting/src/app/contactform/contactform.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {NgIf} from "@angular/common";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-contactform',
+  standalone: true,
+    imports: [
+        NgIf,
+        ReactiveFormsModule,
+        FormsModule
+    ],
+  templateUrl: './contactform.component.html',
+  styleUrl: './contactform.component.scss'
+})
+export class ContactformComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted', this.contactForm.value);
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
+
+}
--- carpainting/src/app/home/home.component.ts
+++ carpainting/src/app/home/home.component.ts
@@ -1,22 +1,21 @@
 import {Component, OnInit, Renderer2} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from "@angular/common";
-import {FormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import 'swiper/swiper-bundle.css';
 import {SliderComponent} from "../slider/slider.component";
+import {ContactformComponent} from "../contactform/contactform.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgOptimizedImage, SliderComponent],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule, NgOptimizedImage, SliderComponent, ContactformComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
   activeLanguage: string = 'en';
   links = ['Home', 'Details', 'Info', 'Contact'];
 
-  constructor(private renderer: Renderer2) {
-  }
 
   setActiveLanguage(language: string) {
     this.activeLanguage = language;
