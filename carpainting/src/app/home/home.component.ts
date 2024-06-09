ef49ed3876d102e524499bcb8c9c7a4da8d388d4
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2024-06-09 14:32:18 +0200

    fixed slider bugs and changes

--- carpainting/src/app/home/home.component.ts
+++ carpainting/src/app/home/home.component.ts
@@ -20,6 +20,23 @@ export class HomeComponent  {
 
   constructor(private renderer: Renderer2) {}
 
+  slides = [
+    { beforeImage: 'assets/images/slide-images/slide-img1.jpg', afterImage: 'assets/images/slide-images/slide-img2.jpg' },
+    { beforeImage: 'assets/images/slide-images/slide-img3.jpg', afterImage: 'assets/images/slide-images/slide-img4.jpg' },
+    // Add more slides as needed
+  ];
+
+  sliderValues = [50, 50]; // Initial slider values for each slide
+  currentSlide = 0;
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide > 0) ? this.currentSlide - 1 : this.slides.length - 1;
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide < this.slides.length - 1) ? this.currentSlide + 1 : 0;
+  }
+
   onMouseMove(event: MouseEvent, overlayId: string): void {
     if (window.innerWidth > 1023) {
       const overlay = document.getElementById(overlayId);
