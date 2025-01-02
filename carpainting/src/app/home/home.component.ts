a7d09b9a8c3a55f5c42bf64ad2f2d00a73e94f1c
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2025-01-02 19:47:38 +0100

    Update

--- carpainting/src/app/home/home.component.ts
+++ carpainting/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, HostListener, Renderer2, ViewChild} from '@angular/core';
+import {Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import 'swiper/swiper-bundle.css';
@@ -17,74 +17,16 @@ import {TranslateModule} from "@ngx-translate/core";
 
 
 })
-export class HomeComponent implements AfterViewInit{
+export class HomeComponent {
   currentLayout: string = 'layout1';
-  selectedSlideIndex: number = 0;
   showFadeIn: boolean = true;
   slides = [
     {before: 'assets/images/slide-images/slide-img1.jpg', after: 'assets/images/slide-images/slide-img2.jpg'},
     {before: 'assets/images/slide-images/slide-img3.jpg', after: 'assets/images/slide-images/slide-img3.jpg'},
   ];
-
-  newSlide = [
-    { src: 'assets/images/cars/img1.webp', alt: 'Image 1' },
-    { src: 'assets/images/cars/img2.webp', alt: 'Image 2' },
-    { src: 'assets/images/cars/img3.webp', alt: 'Image 3' },
-    { src: 'assets/images/cars/img4.webp', alt: 'Image 4' },
-  ];
-
   position: number = 50;
 
   constructor(private renderer: Renderer2) {
-  }
-
-  ngAfterViewInit(): void {
-    this.updateSelectedThumb(this.selectedSlideIndex);
-  }
-
-  updateSelectedThumb(index: number): void {
-    const thumbs = document.querySelectorAll('[id^="carousel-selector-"]');
-    thumbs.forEach((thumb, i) => {
-      if (i === index) {
-        thumb.classList.add('selected');
-      } else {
-        thumb.classList.remove('selected');
-      }
-    });
-  }
-
-  nextSlide(): void {
-    if (this.selectedSlideIndex < this.newSlide.length - 1) {
-      this.setSlide(this.selectedSlideIndex + 1);
-    }
-  }
-
-  prevSlide(): void {
-    if (this.selectedSlideIndex > 0) {
-      this.setSlide(this.selectedSlideIndex - 1);
-    }
-  }
-
-  setSlide(index: number): void {
-    if (index >= 0 && index < this.newSlide.length) {
-      this.selectedSlideIndex = index;
-      this.updateSelectedThumb(index);
-    }
-  }
-
-
-
-  @HostListener('window:resize', ['$event'])
-  onResize(): void {
-    const isMobile = window.innerWidth < 575;
-    const thumbsContainer = document.getElementById('carousel-thumbs');
-    if (thumbsContainer) {
-      if (isMobile) {
-        thumbsContainer.classList.add('mobile-view');
-      } else {
-        thumbsContainer.classList.remove('mobile-view');
-      }
-    }
   }
 
   onSliderInput(event: Event): void {
